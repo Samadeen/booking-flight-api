881e3b5f24292a60245323bedd83498b9be08f6d
Momoh Abdulsamad <samadmomoh@gmail.com>
2022-11-05 09:05:36 +0100

    Index

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const express = require("express");
-const { json } = require("express");
-const flights = require("./controllers/flightController");
-const models = require("./models/Flight");
-const routes = require("./routes/flightRoute");
+const express = require('express');
+const { json } = require('express');
+const flights = require('./controllers/flightController');
+const models = require('./models/Flight');
+const routes = require('./routes/flightRoute');
 
 const app = express();
 
 app.use(json());
 
-app.use("/", routes);
+app.use('/', routes);
 
 const port = process.env.PORT || 3000;
 
